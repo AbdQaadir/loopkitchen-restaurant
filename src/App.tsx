91223401143f754d65d98b3c1e0c5a748f16fa03
Lateef Quadri Olayinka <lateef9816@gmail.com>
2022-06-28 22:48:05 +0100

    feat: setup and login

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Box, ChakraProvider, Text } from "@chakra-ui/react";
+import Signin from "./pages/signin";
 
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const handleLogin = () => setIsSignedIn(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Box w="100%" h="100vh">
+        {isSignedIn ? (
+          <Text>Logged In</Text>
+        ) : (
+          <Signin handleLogin={handleLogin} />
+        )}
+      </Box>
+    </ChakraProvider>
   );
 }
 
---
+++ src/pages/signin.tsx
@@ -0,0 +1,117 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const INITIAL_VALUES = {
+  username: "",
+  password: "",
+};
+type TSignin = {
+  handleLogin: () => void;
+};
+const Signin = ({ handleLogin }: TSignin) => {
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [formValues, setFormValues] = useState(INITIAL_VALUES);
+
+  const isFormValid =
+    Object.values(formValues).filter((item) => item).length > 1;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log({ e: e.target });
+    const { name, value } = e.target;
+
+    setFormValues((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = () => {
+    isFormValid && console.log("submitting...");
+    setError(null);
+    setLoading(true);
+
+    fetch(
+      "https://api.airtable.com/v0/appjWdL7YgpxIxCKA/credenitals?maxRecords=3&view=Grid%20view",
+      {
+        headers: {
+          Authorization: "Bearer keyfXgn8PL6pB3x32",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const isAccountFound = data?.records?.find(
+          (record: any) =>
+            record.fields.username === formValues["username"] &&
+            record.fields.password === formValues["password"]
+        );
+
+        isAccountFound
+          ? handleLogin()
+          : setError("Incorrect login credentials!");
+      })
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  return (
+    <Flex justifyContent="center" alignItems="center" w="100%" h="100%">
+      <Box
+        w="80%"
+        maxW="450px"
+        minH="300px"
+        py={6}
+        px={4}
+        border="1px solid"
+        borderColor="gray.200"
+        borderRadius={3}
+      >
+        <Heading fontSize="1.25rem" my={2}>
+          Login{" "}
+        </Heading>
+        <FormControl mt={4}>
+          <FormLabel htmlFor="username">Username / Email address</FormLabel>
+          <Input
+            id="username"
+            type="text"
+            name="username"
+            placeholder="username"
+            value={formValues["username"]}
+            onChange={handleChange}
+          />
+        </FormControl>
+        <FormControl mt={4}>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <Input
+            id="password"
+            type="password"
+            name="password"
+            placeholder="password"
+            value={formValues["password"]}
+            onChange={handleChange}
+          />
+        </FormControl>
+        <FormControl my={3} isInvalid={!!error}>
+          {error ? <FormErrorMessage>{error}</FormErrorMessage> : <></>}
+        </FormControl>
+
+        <Button
+          colorScheme="orange"
+          isLoading={loading}
+          isDisabled={!isFormValid}
+          onClick={handleSubmit}
+        >
+          Submit
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Signin;
